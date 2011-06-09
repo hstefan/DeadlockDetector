04e921544ca8de3641ce195c3691efd567c76565
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-09 00:27:41 -0300

    Cosmetic change.

--- src/br/ufsm/ddetector/gui/DetectorGUI.java
+++ src/br/ufsm/ddetector/gui/DetectorGUI.java
@@ -13,7 +13,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.SystemColor;
 import java.io.File;
-import java.io.FileFilter;
 import java.util.ArrayList;
 
 import javax.swing.SpringLayout;
