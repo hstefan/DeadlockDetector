ff5ccc5175fb0fa259286d4d6dc8bfe1103ead63
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-09 01:03:07 -0300

    Add support to multiple variable systems.

--- src/br/ufsm/ddetector/GraphBuilder.java
+++ src/br/ufsm/ddetector/GraphBuilder.java
@@ -3,19 +3,20 @@ package br.ufsm.ddetector;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import org.jgraph.graph.DefaultEdge;
 import org.jgrapht.graph.ListenableDirectedGraph;
 
 public class GraphBuilder {
 	ListenableDirectedGraph<Integer, Object> graph;
-	
 	public GraphBuilder(Collection<Operation> operations) {
 		graph = new ListenableDirectedGraph<Integer, Object>(DefaultEdge.class);
 		Iterator<Operation> iter = operations.iterator();
+		Map<Integer, Integer> locks = new TreeMap<Integer, Integer>();
 		Operation op = null;
-		Integer locked = null;
 		Set<Integer> vertex = new HashSet<Integer>();
 		while(iter.hasNext()) {
 			op = iter.next();
@@ -24,18 +25,22 @@ public class GraphBuilder {
 				graph.addVertex(op.getTrans());
 			}
 			if(op.getOp() == Operation.BLOCK) {
-				if(locked == null)
-					locked = op.getTrans();
-				else if (!locked.equals(op.getTrans())){
-					graph.addEdge(op.getTrans(), locked);
+				if(!locks.containsKey(op.getVar()))
+					locks.put(op.getVar(), op.getTrans());
+				else {
+					Integer trans = locks.get(op.getVar());
+					if(!trans.equals(op.getTrans())) {
+						graph.addEdge(op.getTrans(), trans);
+					}
 				}
 			} else if(op.getOp() == Operation.UNBLOCK) {
-				if(locked.equals(op.getTrans())) {
-					locked = null;
-					Set<Object> arriving = graph.incomingEdgesOf(locked);
+				if(locks.get(op.getVar()).equals(op.getTrans())) {
+					Set<Object> arriving = graph.incomingEdgesOf(locks.get(op.getVar()));
+					locks.remove(op.getVar());
 					for(Object obj : arriving) {
 						graph.removeEdge(obj);
 					}
+					
 				}
 			} 
 		}
